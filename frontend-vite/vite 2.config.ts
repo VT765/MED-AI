a4adbabafca6519d7191b7f8468ff42c79f9ee73
Varun Thacker <varunthacker4@gmail.com>
2026-02-06 22:11:11 +0530

    removed extra files

--- frontend-vite/vite 2.config.ts
+++ frontend-vite/vite 2.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
@@ -19,3 +21,5 @@ export default defineConfig({
     },
   },
 });
+
+
